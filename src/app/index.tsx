96df0e972fb079f85d8403444a1e38c34f53c69e
sagar-nigam <sagar.nigam.gr@gmail.com>
2024-08-25 23:38:51 +0200

    wleome page added with button animation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import Animated, { FadeIn, FadeInDown, FadeOut } from 'react-native-reanimated';
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <ImageBackground source={require('../../assets/images/home-1.jpg')} resizeMode="cover" style={styles.backgroundImage}>
+        <Animated.View entering={FadeInDown.delay(100).springify()} style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.primaryButton}>
+            <Text style={styles.buttonText}>Get Started</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +20,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 'auto',
+  },
+  primaryButton: {
+    backgroundColor: '#319AE5',
+    width: '60%',
+    height: '25%',
+    borderRadius: 25,
+    alignItems: 'center',
+  },
+  buttonText: {
+    padding: 5,
+    fontSize: 30,
+    color: 'white',
+  }
 });
