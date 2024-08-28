53643d6003851031b82c55cf1961cc35fbcdc83f
sagar-nigam <sagar.nigam.gr@gmail.com>
2024-08-28 22:36:32 +0200

    status bar area issue solved

--- src/app/exerciseHome.tsx
+++ src/app/exerciseHome.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import NavigationBar from "../components/navigationBar";
 
-export default function exerciseHome() {
+export default function ExerciseHome() {
   return (
-    <View>
-      <Text>exerciseHome</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" backgroundColor="#09131F" />
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.text}>Exercise page</Text>
+        <NavigationBar />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+  },
+  exerciseContainer: {
+    paddingTop: 60,
+    height: '100%',
+    width: '100%',
+    backgroundColor: "#09131F",
+  },
+  text: {
+    color: "white", // Ensures the text is visible on a dark background
+  },
+});
