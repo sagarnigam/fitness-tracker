0cbdd589b09b284defdd28ce8e4ae4580f883a49
sagar-nigam <sagar.nigam.gr@gmail.com>
2024-09-03 18:45:39 +0200

    changes

--- src/app/workoutRoutine.tsx
+++ src/app/workoutRoutine.tsx
@@ -1,12 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
 const WorkoutRoutine = () => {
-  return (
-    <View>
-      <Text>workoutRoutine</Text>
-    </View>
-  )
-}
+  const { workoutDetailsParams } = useLocalSearchParams();
+  const programData = workoutDetailsParams ? JSON.parse(workoutDetailsParams) : null;
+  const router = useRouter();
 
-export default WorkoutRoutine
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" backgroundColor="#09131F" />
+      <View style={styles.workoutProgramsContainer}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back-circle" size={40} color="white" />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>{programData.name}</Text>
+        </View>
+        <View style={styles.exercisesListContainerContainer}>
+            <Text>{programData.name}</Text> 
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+  },
+  workoutProgramsContainer: {
+    padding: 15,
+    paddingTop: 60,
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#09131F",
+  },
+  headerContainer: {
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 40,
+    color: "white",
+    fontStyle: "italic",
+  },
+  exercisesListContainerContainer: {
+    height: "82%",
+    marginBottom: 20,
+  },
+});
+
+export default WorkoutRoutine;
