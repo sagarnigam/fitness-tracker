e51b57c5b31d6603c0869c538cf6b9629b7efc9c
sagar-nigam <sagar.nigam.gr@gmail.com>
2024-09-09 19:21:27 +0200

    Profile screen progress

--- src/app/(tabs)/exerciseCategoryHome.tsx
+++ src/app/(tabs)/exerciseCategoryHome.tsx
@@ -8,7 +8,6 @@ import {
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import NavigationBar from "../../components/navigationBar";
 import { EXERCISE_CATEGORY } from "../../mock-data/exercise-list";
 import ExerciseCategoryCard from "../../components/exerciseCategoryCard";
 import { useRouter } from "expo-router";
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" backgroundColor="#09131F" />
+      <View style={styles.profileContainer}>
+        <ScrollView style={styles.scrollContentContainer}>
+          <View style={styles.homeHeaderContainer}>
+            <Text style={styles.headerText}>Profile</Text>
+          </View>
+
+          </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+  },
+  profileContainer: {
+    display: "flex",
+    padding: 15,
+    paddingTop: 60,
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#09131F",
+  },
+  scrollContentContainer: {
+    flex: 1,
+  },
+  navigationContainer: {
+    flex: 0.065,
+  },
+  homeHeaderContainer: {
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 40,
+    color: "white",
+    fontStyle: "italic",
+  },
+  subheaderText: {
+    fontSize: 28,
+    color: "#319AE5",
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+  },
+});
